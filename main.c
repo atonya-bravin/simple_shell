762418e4f4f8bd61c9b3002ff058c871c77fa838
bravin-atonya <bravinatonya.brani@gmail.com>
2022-11-17 09:38:27 -0800

    inclussion of main file

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * main - starting point of the project
+ *
+ * @argc :the number of parameters provided by the user
+ * @argv :the argument list provided by the user
+ *
+ * Return: 0(success) or 1(fail)
+ */
+
+int main(int argc, int argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
